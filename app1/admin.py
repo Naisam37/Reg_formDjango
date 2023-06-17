2f7e807317938bfca1e68847182f3d2612105a2c
Naisam37 <naisamta123@gmail.com>
2023-06-17 14:22:21 +0530

    'update'

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+# from .models import Naaykkal
 # Register your models here.
+# admin.site.register(Naaykkal)
+
+# class NaaykkalForm(admin.ModelAdmin):
+    
--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# class Naaykkal (models.Model):
+#         uname =models.CharField(max_length=50)
+#         email =models.EmailField()
+#         pass1 =models.CharField(max_length=25)
+#         pass2 =models.CharField(max_length=25)
+#         age =models.IntegerField()
+        
+        
+
+
+    
